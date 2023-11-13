83ee2b76100a50a4cd344e32d68600e9c5b2b920
hasnat-shohag <hasnat.ru.ice19@gmail.com>
2023-11-14 01:05:49 +0600

    loader and conditional rendering according to api call are added

--- src/components/Todo/Todo.js
+++ src/components/Todo/Todo.js
@@ -1,34 +1,47 @@
 import { useEffect, useState, useRef } from "react";
+import LoadingSpinner from "../Loader/LoadinSpinner";
 
 const Todo = () => {
 	const [value, setValue] = useState([]);
 	const isDataFetched = useRef(false);
+	const [isLoading, setIsLoading] = useState(false);
+	const [errorMessage, setErrorMessage] = useState("");
 
 	const apiFun = async () => {
 		let responseData = await fetch(
 			"https://jsonplaceholder.typicode.com/todos/"
 		)
 			.then((res) => res.json())
-			.catch((err) => console.log("Errors:::: ", err));
+			.catch((err) => {
+				setErrorMessage("Unable to fetch user list");
+			});
 		return responseData;
 	};
 
 	useEffect(() => {
+		setIsLoading(true);
 		if (isDataFetched.current) return;
 		apiFun().then((res) => {
 			setValue(res);
+			setIsLoading(false);
 		});
 		isDataFetched.current = true;
 	}, []);
-	const todoList = value;
+
+	if (isLoading) {
+		return <LoadingSpinner />;
+	}
+
+	if (errorMessage) {
+		return <div>{errorMessage}</div>;
+	}
+
 	return (
 		<div>
 			<ul>
-				{console.log(typeof todoList)}
-				{todoList.length > 0 &&
-					todoList.map((val) => {
-						return <li key={val.id}>{val.title}</li>;
-					})}
+				{value.map((val) => {
+					return <li key={val.id}>{val.title}</li>;
+				})}
 			</ul>
 		</div>
 	);
